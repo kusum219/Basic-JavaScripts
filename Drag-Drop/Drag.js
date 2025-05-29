1ef09b0f997207d09c775004ffaa237209a3342f
kusum219 <mgkusum55@gmail.com>
2025-05-29 08:34:02 +0545

    final

--- Drag-Drop/Drag.js
+++ Drag-Drop/Drag.js
@@ -3,26 +3,24 @@ let rightBox = document.getElementById("right");
 let leftBox = document.getElementById("left");
 
 for(list of lists){
-    list.addEventListener("dragstart", function(e){
-      let selected = e.target;
-      
-      
-      rightBox.addEventListener("dragover",function(e){
-        e.preventDefault();
-      })
-      rightBox.addEventListener("drop",function(e){
-        rightBox.appendChild(selected);
-        selected = null;
-      });
-      
+  list.addEventListener("dragstart",(e)=>{
+    let selected = e.target;
 
-
-        leftBox.addEventListener("dragover",function(e){
-        e.preventDefault();
-      })
-      leftBox.addEventListener("drop",function(e){
-        leftBox.appendChild(selected);
-        selected = null;
-      });
+    rightBox.addEventListener("dragover",(e)=>{
+      e.preventDefault();
     })
+    rightBox.addEventListener("drop",()=>{
+      rightBox.appendChild(selected);
+      selected = null;
+    })
+
+
+    leftBox.addEventListener("dragover",(e)=>{
+      e.preventDefault();
+    })
+    leftBox.addEventListener("drop",()=>{
+    leftBox.appendChild(selected);
+    selected = null
+  })
+  })
 }
